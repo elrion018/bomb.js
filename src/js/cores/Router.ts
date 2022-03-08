5a94009992f0d35d9a9403681d514fc8ca82518b
karl <elrion018@gmail.com>
2022-03-09 00:24:32 +0900

    feat : router 클래스 완성

--- src/js/cores/Router.ts
+++ src/js/cores/Router.ts
@@ -1,11 +1,13 @@
 import { Component } from "./";
 
-interface RouterRoutes extends Object {
-  [route: string]: Component;
+interface RouterRoutes {
+  [route: string]: typeof Component;
 }
 
+type RouterRoutesFromObject = RouterRoutes & Object;
+
 export class Router {
-  routes: RouterRoutes;
+  routes: RouterRoutesFromObject;
   targetSelector: string;
 
   /**
@@ -29,8 +31,31 @@ export class Router {
     });
   }
 
+  /**
+   * 매개변수로 받은 route에 따라 대응하는 컴포넌트 인스턴스를 만드는 메소드
+   */
   loadRoute(route: string) {
-    if (this.routes.hasOwnProperty(route)) {
+    if (this.routes.hasOwnProperty(route))
+      new this.routes[route](this.targetSelector, null, null);
+  }
+
+  /**
+   * 매겨변수로 받은 route로 replace하고 이에 따라 라우팅하는 메소드
+   */
+  replaceRoute(route: string) {
+    window.history.replaceState({ route }, route, `/${route}`);
+  }
+
+  /**
+   * 매개변수로 받은 route를 push하고 이에 따라 라우팅하는 메소드
+   */
+  pushRoute(route: string) {
+    const nowRoute = history.state.route;
+
+    window.history.pushState({ route }, route, `/${route}`);
+
+    if (nowRoute !== route) {
+      this.loadRoute(route);
     }
   }
 }
